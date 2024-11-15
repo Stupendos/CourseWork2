4abd249c5509cee751ad6d92be7e3c989951e703
Stupendos <topakovdenis671@gmail.com>
2024-11-15 23:23:44 +0700

    Start

---
+++ src/main/java/com/example/work/course2/MyCourseWork2/MyCourseWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.work.course2.MyCourseWork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCourseWork2Application.class, args);
+	}
+
+}
